256c0047847e12395757e223340824b5ae7d5ddf
Lu.Teserat <dobry.tester.pl@gmail.com>
2024-07-12 00:02:24 +0200

    Test expans : click buttion 1 + verfication visibility

--- src/test/my_web_test/SeleniumWelcomeTest.java
+++ src/test/my_web_test/SeleniumWelcomeTest.java
@@ -1,6 +1,8 @@
 package my_web_test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.junit.jupiter.api.Test;
 
@@ -14,8 +16,32 @@ public class SeleniumWelcomeTest {
         driver.manage().
                 window().
                 maximize();
+        //driver.get("http://127.0.0.1:5500/index.html"); //local host
         driver.get("https://teserat.github.io/welcome/");
 
+        By cssID = By.cssSelector("#img1");
+        driver.findElement(cssID);
+
+        By cssIDClickableButton = By.cssSelector("#testID1");
+        driver.findElement(cssIDClickableButton).click();
+
+        WebElement ImageOneIsVisibleVerification = driver.findElement(By.cssSelector("img[alt='Obrazek 1"));
+
+        if (ImageOneIsVisibleVerification.isDisplayed()){
+            System.out.println("Obrazek 1 jest widoczny");
+        }else {
+            System.out.println("Obrazek 1 nie jest widoczny");
+        }
+
+
+
+        try {
+            Thread.sleep(1000); // 5000 milisekund = 5 sekund
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         driver.quit();
     }
+
+    //ToDo tests WebDriverWait
 }
