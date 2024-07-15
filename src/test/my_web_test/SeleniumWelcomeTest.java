1536cd09345af2ad652402cc6bc45b745f9100c6
Lu.Teserat <dobry.tester.pl@gmail.com>
2024-07-15 23:36:57 +0200

    some changes. extra selectors by tag + some changes

--- src/test/my_web_test/SeleniumWelcomeTest.java
+++ src/test/my_web_test/SeleniumWelcomeTest.java
@@ -5,13 +5,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.junit.jupiter.api.Test;
+import java.util.List;
 
 public class SeleniumWelcomeTest {
-    // testy na specjalnie przygotowanej stronie
+    // tests on a specially prepared website
     @Test
     public void SeleniumWelcomeTest() {
         WebDriverManager.chromedriver().setup();
-        // Inicjalizacja Chrome options
+        //Initialize Chrome options
         ChromeDriver driver = new ChromeDriver();
         driver.manage().
                 window().
@@ -20,28 +21,42 @@ public class SeleniumWelcomeTest {
         driver.get("https://teserat.github.io/welcome/");
 
         By cssID = By.cssSelector("#img1");
-        driver.findElement(cssID);
+        driver.findElement(cssID); // finding selector
 
-        By cssIDClickableButton = By.cssSelector("#testID1");
+        By cssIDClickableButton = By.cssSelector("#testID1"); // 1 2 3 change to see if visible or not
         driver.findElement(cssIDClickableButton).click();
 
         WebElement ImageOneIsVisibleVerification = driver.findElement(By.cssSelector("img[alt='Obrazek 1"));
 
-        if (ImageOneIsVisibleVerification.isDisplayed()){
-            System.out.println("Obrazek 1 jest widoczny");
-        }else {
-            System.out.println("Obrazek 1 nie jest widoczny");
+        if (ImageOneIsVisibleVerification.isDisplayed()) {
+            System.out.println("Obrazek 1 is visible");
+        } else {
+            System.out.println("Obrazek 1 is not visible");
         }
 
+        By inputLocator = By.tagName("input");
+        WebElement input = driver.findElement(inputLocator); // first input in code
+
+        List<WebElement> inputs = driver.findElements(inputLocator);  // list of all imputs in code
+        System.out.println(inputs.size());
+
+        driver.findElement(By.cssSelector("body > button")).click();
 
 
-        try {
-            Thread.sleep(1000); // 5000 milisekund = 5 sekund
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        sleep();
         driver.quit();
     }
 
+
+    public static void sleep() {
+        try {
+            Thread.sleep(2000); // 5000 milisekund = 5 sekund
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
     //ToDo tests WebDriverWait
+    //ToDo seperate 2 tests or more
 }
+
+
