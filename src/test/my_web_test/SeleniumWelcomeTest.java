941efa5c4b0ef38b424c850e756243da3f427786
Lu.Teserat <dobry.tester.pl@gmail.com>
2024-07-17 23:01:32 +0200

    adding extra selectors and make some clean up

--- src/test/my_web_test/SeleniumWelcomeTest.java
+++ src/test/my_web_test/SeleniumWelcomeTest.java
@@ -20,16 +20,40 @@ public class SeleniumWelcomeTest {
         //driver.get("http://127.0.0.1:5500/index.html"); //local host
         driver.get("https://teserat.github.io/welcome/");
 
-        By cssID = By.cssSelector("#img1");
-        driver.findElement(cssID); // finding selector
 
-        By cssIDClickableButton = By.cssSelector("#testID1"); // 1 2 3 change to see if visible or not
+
+
+
+        By cssID = By.id("img1");  // by html element
+        //By cssID = By.cssSelector("#img1");  // use a css selector to do the same with #
+        driver.findElement(cssID); // finding selector in code for id (if its no selector, test fail)
+
+        By firstName = By.name("firstName"); // by name attribute
+        WebElement name = driver.findElement(firstName);
+        By lastName = By.cssSelector("[name='lastName']"); // same by css selector
+        WebElement surname = driver.findElement(lastName);
+
+        By emotsList = By.className("emoticon-list"); //by class
+        //By emotsList = By.cssSelector("[class=emoticon-list"); // same by css selector
+        driver.findElement(emotsList);
+
+
+        By link1 = By.linkText("Pixabay Playlists");
+        By link2 = By.partialLinkText("Jango");
+        driver.findElement(link1);
+        driver.findElement(link2);
+
+
+        //-------------------------------------  test to split
+
+        By cssIDClickableButton = By.cssSelector("#testID1");
         driver.findElement(cssIDClickableButton).click();
 
+        //save as WebElement object
         WebElement ImageOneIsVisibleVerification = driver.findElement(By.cssSelector("img[alt='Obrazek 1"));
 
         if (ImageOneIsVisibleVerification.isDisplayed()) {
-            System.out.println("Obrazek 1 is visible");
+            System.out.println("Obrazek 1 is visible"); // If button 1 is clicked then "Obrazek 1" is visible
         } else {
             System.out.println("Obrazek 1 is not visible");
         }
@@ -43,14 +67,14 @@ public class SeleniumWelcomeTest {
         driver.findElement(By.cssSelector("body > button")).click();
 
 
-        sleep();
+        //sleep();
         driver.quit();
     }
 
 
     public static void sleep() {
         try {
-            Thread.sleep(2000); // 5000 milisekund = 5 sekund
+            Thread.sleep(2000); // 5000 miliseconds = 5 seconds
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
