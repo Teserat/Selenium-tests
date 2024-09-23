858747a19d8cd1ac8a6d2285d498fb26e97da661
Lu.Teserat <dobry.tester.pl@gmail.com>
2024-09-23 23:27:11 +0200

    Add class WaitTest and 2 tests

--- src/test/my_web_test/WaitTest.java
+++ src/test/my_web_test/WaitTest.java
@@ -1,15 +1,20 @@
 package my_web_test;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.List;
-import java.util.NoSuchElementException;
 
 public class WaitTest {
 
@@ -30,6 +35,7 @@ public class WaitTest {
         basicPageLink.click();
         WebElement elementsPageLink = driver.findElement(By.linkText("TestSide6 - Wait test"));
         elementsPageLink.click();
+
         //TestUtils.sleep(4000);
         //using list - will not throw an exception if the element does not exist (like normal find element)
         List<WebElement> elements = driver.findElements(By.xpath("//div[text()='Hello']"));
@@ -39,12 +45,13 @@ public class WaitTest {
 
     // test with wait sleep
     @Test
-    public void verificationIfTextIsInvisibleWithSleep() {
+    public void verificationVisibilityTextWithSleep() {
 
         WebElement basicPageLink = driver.findElement(By.linkText("Rozchodniak"));
         basicPageLink.click();
         WebElement elementsPageLink = driver.findElement(By.linkText("TestSide6 - Wait test"));
         elementsPageLink.click();
+
         TestUtils.sleep(4000);
         WebElement test = driver.findElement(By.xpath("//div[text()='Hello']"));
         System.out.println(test.getText());
@@ -52,10 +59,55 @@ public class WaitTest {
     }
 
     //test with Implicit - whole test wait on every step
+    @Test
+    public void verificationVisibilityTextWithImplicit() {
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        WebElement basicPageLink = driver.findElement(By.linkText("Rozchodniak"));
+        basicPageLink.click();
+        WebElement elementsPageLink = driver.findElement(By.linkText("TestSide6 - Wait test"));
+        elementsPageLink.click();
+
+
+        WebElement test = driver.findElement(By.xpath("//div[text()='Hello']"));
+        System.out.println(test.getText());
+        Assert.assertEquals(test.getText(), "Hello");
+    }
 
     //test with WebDriverWait (child FluentWait)
+    @Test
+    public void verificationVisibilityTextWithWebDriverWait() {
 
+        WebElement basicPageLink = driver.findElement(By.linkText("Rozchodniak"));
+        basicPageLink.click();
+        WebElement elementsPageLink = driver.findElement(By.linkText("TestSide6 - Wait test"));
+        elementsPageLink.click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[text()='Hello']")));
+
+        WebElement test = driver.findElement(By.xpath("//div[text()='Hello']"));
+        System.out.println(test.getText());
+        Assert.assertEquals(test.getText(), "Hello");
+    }
     //test with FluentWait - no exceptions inside, like in WebDriverWait
+    @Test
+    public void verificationVisibilityTextWithFluentWait() {
+
+        WebElement basicPageLink = driver.findElement(By.linkText("Rozchodniak"));
+        basicPageLink.click();
+        WebElement elementsPageLink = driver.findElement(By.linkText("TestSide6 - Wait test"));
+        elementsPageLink.click();
+
+        FluentWait<WebDriver> wait = new FluentWait<>(driver);
+        wait.withTimeout(Duration.ofSeconds(10));
+        wait.ignoring(NoSuchElementException.class);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[text()='Hello']")));
+
+        WebElement test = driver.findElement(By.xpath("//div[text()='Hello']"));
+        System.out.println(test.getText());
+        Assert.assertEquals(test.getText(), "Hello");
+    }
 
     //ExpectedConditions
 
